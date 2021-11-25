b4e329b239453baf4df8b00705b561243cc9e0d0
mamo1986 <94875025+mamo1986@users.noreply.github.com>
2021-11-24 20:41:55 -0500

    Update p2.java
    i add it

--- src/demop2/p2.java
+++ src/demop2/p2.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println(" demo 2 ...");
 	System.out.println("love ");
 	System.out.println("mahnoush ");
+	System.out.println("add randome comment  ");
 
 }
 }
